2032a92a848d8070b872ea2dfa3e8781123c045c
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-04-30 20:00:40 +0300

    fix: fixed implementation of "switch to dark theme" functionality

--- online-zoo/pages/Landing/js/script.js
+++ online-zoo/pages/Landing/js/script.js
@@ -1,68 +1,72 @@
 // собираем все якоря; устанавливаем время анимации и количество кадров
 const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-    animationTime = 200,
-    framesCount = 20;
+	animationTime = 200,
+	framesCount = 20;
 
-anchors.forEach(function(item) {
-  // каждому якорю присваиваем обработчик события
-  item.addEventListener('click', function(e) {
-    // убираем стандартное поведение
-    e.preventDefault();
-    
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-    
-    // запускаем интервал, в котором
-    let scroller = setInterval(function() {
-      // считаем на сколько скроллить за 1 такт
-      let scrollBy = coordY / framesCount;
-      
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
-  });
+anchors.forEach(function (item) {
+	// каждому якорю присваиваем обработчик события
+	item.addEventListener('click', function (e) {
+		// убираем стандартное поведение
+		e.preventDefault();
+
+		// для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+		let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+		// запускаем интервал, в котором
+		let scroller = setInterval(function () {
+			// считаем на сколько скроллить за 1 такт
+			let scrollBy = coordY / framesCount;
+
+			// если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+			// и дно страницы не достигнуто
+			if (
+				scrollBy > window.pageYOffset - coordY &&
+				window.innerHeight + window.pageYOffset < document.body.offsetHeight
+			) {
+				// то скроллим на к-во пикселей, которое соответствует одному такту
+				window.scrollBy(0, scrollBy);
+			} else {
+				// иначе добираемся до элемента и выходим из интервала
+				window.scrollTo(0, coordY);
+				clearInterval(scroller);
+			}
+			// время интервала равняется частному от времени анимации и к-ва кадров
+		}, animationTime / framesCount);
+	});
 });
-// менюбургер
+// меню-бургер
 const header__burger = document.querySelector('.icon-menu');
 const header_menu = document.querySelector('.menu_body');
 const back = document.querySelector('body');
 const header__list = document.querySelector('.menu_list');
 
-header__burger.onclick = function(){
-    header__burger.classList.toggle('active');
-    header_menu.classList.toggle('active');
-    back.classList.toggle('lock');
-}
+header__burger.onclick = function () {
+	header__burger.classList.toggle('active');
+	header_menu.classList.toggle('active');
+	back.classList.toggle('lock');
+};
 
 header__list.onclick = function () {
-    header__list.classList.remove('active');
-    back.classList.toggle('lock');
-}
+	header__list.classList.remove('active');
+	back.classList.toggle('lock');
+};
+
 // переключатель темы
-
+  
+	const map = document.querySelector('.map__map img');
+	const button = document.querySelector('.header_check img');
+	const wrapper = document.getElementById('theme_css');
+  button.addEventListener('click', switchTheme)
 function switchTheme() {
-  const check = document.getElementById('theme_css').classList[0] === 'light';
-const map = document.querySelector('.map__map img')
-  const element = document.getElementById('theme_css');
-  if (check) {
-   element.href = './css/style-dark.css?v=2';
-   element.classList.remove('light')
-   element.classList.add('dark');
-   map.src = '../../assets/Landing/images/map-dark.png'
-  } else {
-   element.href = './css/style-light.css?v=2';
-   element.classList.remove('dark')
-   element.classList.add('light');
-   map.src = '../../assets/Landing/images/map.png'
-  }
-
+	if (wrapper.classList.contains('theme-light')) {
+		wrapper.classList.remove('theme-light');
+		wrapper.classList.add('theme-dark');
+		map.src = '../../assets/Landing/images/map-dark.png';
+		button.src = '../../assets/Landing/images/swich-dark.png';
+	} else {
+		wrapper.classList.remove('theme-dark');
+		wrapper.classList.add('theme-light');
+		map.src = '../../assets/Landing/images/map.png';
+		button.src = '../../assets/Landing/images/swich-light.png';
+	}
 }
