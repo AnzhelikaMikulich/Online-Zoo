d73f7d7241ffb882624c5bb50db8a41949d6daed
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-05-04 05:02:13 +0300

    feat:add FavoriteAnimal Slider

--- online-zoo/pages/Landing/js/script.js
+++ online-zoo/pages/Landing/js/script.js
@@ -1,4 +1,4 @@
-// навигация по странице
+// Navigation
 const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
 	animationTime = 200,
 	framesCount = 20;
@@ -22,7 +22,7 @@ anchors.forEach(function (item) {
 	});
 });
 
-// меню-бургер
+// Burger-menu
 const header__burger = document.querySelector('.icon-menu');
 const header_menu = document.querySelector('.menu_body');
 const back = document.querySelector('body');
@@ -39,16 +39,16 @@ header__list.onclick = function () {
 	back.classList.toggle('lock');
 };
 
-// переключатель темы
+// Swich Theme
 const button = document.querySelector('.header_check img');
 let darkMode = localStorage.getItem('darkMode');
 const map = document.querySelector('.map__map img');
 const wrapper = document.getElementById('theme_css');
-if(darkMode === 'true') {
-  wrapper.classList.remove('theme-light');
-  wrapper.classList.add('theme-dark');
-  map.src = '../../assets/Landing/images/map-dark.png';
-  button.src = '../../assets/Landing/images/swich-dark.png';
+if (darkMode === 'true') {
+	wrapper.classList.remove('theme-light');
+	wrapper.classList.add('theme-dark');
+	map.src = '../../assets/Landing/images/map-dark.png';
+	button.src = '../../assets/Landing/images/swich-dark.png';
 }
 button.addEventListener('click', switchTheme);
 function switchTheme() {
@@ -57,46 +57,73 @@ function switchTheme() {
 		wrapper.classList.add('theme-dark');
 		map.src = '../../assets/Landing/images/map-dark.png';
 		button.src = '../../assets/Landing/images/swich-dark.png';
-    localStorage.setItem('darkMode', 'true');
+		localStorage.setItem('darkMode', 'true');
 	} else {
 		wrapper.classList.remove('theme-dark');
 		wrapper.classList.add('theme-light');
 		map.src = '../../assets/Landing/images/map.png';
 		button.src = '../../assets/Landing/images/swich-light.png';
-    localStorage.setItem('darkMode', 'false');
+		localStorage.setItem('darkMode', 'false');
 	}
 }
 
-//СЛАЙДЕР Pets in Zoo
-let ofset = 0;
-const slider_body = document.querySelector('#pets-in-zoo');
-const slider_next = document.querySelector('.slider_next_pets-in-zoo');
-const slider_prev = document.querySelector('.slider_prev_pets-in-zoo');
+// Input-range
+const input = document.querySelectorAll('input[type="range"]');
+const output = document.querySelectorAll('.label-title span');
+input.forEach((e, i) =>
+	e.addEventListener('input', () => {
+		let newValue = e.value;
+		output[i].innerHTML = `0${newValue}/`;
+	})
+);
 
-slider_next.addEventListener('click', function () {
+// FavoriteAnimal Slider//
+const favSlider = document.querySelector('.favorite_animal_slider_img');
+const favInput = document.querySelector('.input-favorite input');
+const favOutput = document.querySelector('.input-favorite span');
+const favItems = favSlider.querySelectorAll('.slider_item_favorite');
+
+favSlider.addEventListener('click', (e) => {
+	if (e.target.dataset.index !== undefined) {
+		favInput.value = e.target.dataset.index;
+		favOutput.innerHTML = `0${e.target.dataset.index}/`;
+		setLocation(e.target.dataset.index);
+	}
+});
+
+favInput.addEventListener('change', (e) => {
+	if (e.target.value <= e.target.max && e.target.value >= e.target.min) {
+		setLocation(e.target.value);
+	}
+});
+function setLocation(i) {
+	favItems.forEach((el) => {
+		if (el.classList.contains('slider_item_favorite_big')) {
+			el.classList.remove('slider_item_favorite_big');
+		}
+		const elWidth = el.offsetWidth + parseInt(getComputedStyle(el).marginRight.match(/\d+/)[0]);
+		el.style.transform = `translateX(${-(i - 1) * elWidth}px)`;
+	});
+	favItems[i - 1].classList.add('slider_item_favorite_big');
+}
+
+//Pets in Zoo Slider
+let ofset = 0;
+const petsSlider = document.querySelector('#pets-in-zoo');
+const petsSliderNext = document.querySelector('.slider_next_pets-in-zoo');
+const petsSliderPrev = document.querySelector('.slider_prev_pets-in-zoo');
+
+petsSliderNext.addEventListener('click', function () {
 	ofset = ofset + 297;
 	if (ofset > 1200) {
 		ofset = 0;
 	}
-	slider_body.style.left = -ofset + 'px';
+	petsSlider.style.left = -ofset + 'px';
 });
-slider_prev.addEventListener('click', function () {
+petsSliderPrev.addEventListener('click', function () {
 	ofset = ofset - 308;
 	if (ofset < 0) {
 		ofset = 1200;
 	}
-	slider_body.style.left = -ofset + 'px';
+	petsSlider.style.left = -ofset + 'px';
 });
-
-// работа Input-range
-
-const input = document.querySelectorAll('input[type="range"]');
-const output = document.querySelectorAll('.label-title span')
-
-
-input.forEach((e,i) =>
-	e.addEventListener('input', () => {
-		var newValue = e.value;
-		output[i].innerHTML = `0${newValue}/`;
-	})
-);
